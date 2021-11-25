2e63112ed3764a0656a043255e0f87dea9aa9dec
nazar <nazar@DESKTOP-MOV89AK>
2021-11-25 13:43:52 -0500

    Bils Game

---
+++ src/buffalobills/NumberOfTeams.java
@@ -0,0 +1,20 @@
+package buffalobills;
+
+public class NumberOfTeams {
+
+	
+	public static void main(String[] args) {
+		getInfo("Bils team", 12);
+		
+	}
+	String name = "Bills team";
+	int getTotal = 12;
+	
+		public static void getInfo(String name, int getTotal) {
+			System.out.println(name);
+			System.out.println(getTotal);
+		}
+
+	}
+
+
